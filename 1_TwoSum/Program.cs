9f672cfbb2df0c2b5c52627c3324ad13ece7a344
Tymur Nesterenko <timur.nes@citrusdev.com.ua>
2019-09-18 10:00:04 +0300

    1 - Two Sum

---
+++ 1_TwoSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1_TwoSum {
+    class Program {
+        static void Main(string[] args) {
+            var result = TwoSum(new[] { 2, 11, 7, 15 }, 9);
+        }
+
+        public static int[] TwoSum(int[] nums, int target) {
+            var comp = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++) {
+                var value = nums[i];
+                var complement = target - value;
+                if (comp.ContainsKey(complement)) {
+                    return new[] { comp[complement], i };
+                }
+
+                comp[value] = i;
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
--- LeetCode/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace LeetCode {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
