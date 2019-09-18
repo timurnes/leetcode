8cf3027182538bcd05e9eb8f49b091a00a9f7eae
Tymur Nesterenko <timur.nes@citrusdev.com.ua>
2019-09-18 10:01:55 +0300

    9. Palindrome Number

---
+++ 9_PalindromeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _9_PalindromeNumber {
+    class Program {
+        static void Main(string[] args) {
+            var res1 = IsPalindrome(121);
+            var res2 = IsPalindrome(-121);
+            var res3 = IsPalindrome(12321);
+        }
+
+        public static bool IsPalindrome(int x) {
+            if (x < 0 || (x % 10 == 0 && x != 0)) {
+                return false;
+            }
+
+            int revertedNumber = 0;
+            while (x > revertedNumber) {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == revertedNumber || x == revertedNumber / 10;
+        }
+    }
+}
